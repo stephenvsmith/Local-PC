cbb3d1d68e06fd9a558b726666a2f5c38a95a18d
stephenvsmith <stephensmith13424@gmail.com>
2020-01-17 17:24:48 -0800

    Simplifying the folder structure

---
+++ R/pc_skeleton.R
@@ -0,0 +1,40 @@
+#' PC Algorithm for Skeleton (for Local PC)
+#'
+#' @param dataset default is NULL, only used when we are using sample version and have data
+#' @param C_tilde default is NULL, used in the local algorithm with each clique being considered forming a complete graph
+#' @param cor.mat default is NULL, the correlation matrix for the nodes (population algorithm)
+#' @param lmax the maximum size of the separating sets that we will consider (default is 3)
+#' @param verbose messages concerning the operation of the algorithm (default is TRUE)
+#' @param tol the significance level (default is 0.005)
+#'
+#' @export
+
+pc_skeleton <- function(dataset=NULL,C_tilde=NULL,true_dag=NULL,
+                        pop=TRUE,lmax=3,
+                        verbose=TRUE,tol=0.01){
+
+  # Initial setup
+  var_list <- pc_skel_setup(dataset,true_dag,C_tilde,
+                            pop,lmax,
+                            verbose,tol)
+
+  # Initialize size of subset
+  var_list[["l"]] = -1
+  # Initialize adjacency matrix for finished graph
+  var_list[["C"]] <- C_tilde
+
+  ### loop over all l values (until lmax)
+  while (var_list[["l"]] < var_list[["lmax"]]){
+
+    var_list[["l"]] <- var_list[["l"]] + 1
+
+    if (verbose) cat("l value is: ",var_list[["l"]],"\n")
+
+    ### loop over all i values
+    for (i in 1:var_list$p){
+      var_list <- skeleton_target(i,var_list)
+    }
+
+  }
+  return(list("adjacency"=var_list$C,"sep_sets"=var_list$S,"p_vals"=var_list$p.vals))
+}
---
+++ R/skeleton_target.R
@@ -0,0 +1,88 @@
+### This is the other loop for the PC Algorithm step for finding the skeleton
+
+skeleton_target <- function(i,var_list){
+
+  var_list[["i"]] <- i
+  if (var_list$verbose) cat("i value is: ",i,"\n")
+
+  # Determine adjacent values for possible j
+
+  var_list[["jvals"]] <- which(var_list[["C"]][i,] != 0)
+
+  ### loop over all adjacent j values
+  for (j in var_list$jvals){
+    var_list <- check_neighbor(j,var_list)
+  }
+
+  return(var_list)
+}
+
+#### Check potential neighbor for target during PC Algorithm skeleton step
+
+check_neighbor <- function(j,var_list){
+
+  var_list[["j"]] <- j
+  if (var_list$verbose) cat("j value is: ",j,"\n")
+
+  # (i,j) is the pair we are considering
+  # adj is the remaining adjacent nodes to i not equal to j
+  adj <- find_neighbors(var_list$i,var_list$true_dag)
+  var_list$adj <- adj[adj!=j]
+
+  if (length(var_list$adj)>=var_list$l){ # Makes sure there is a valid amount of adjacent nodes for each step
+
+    var_list <- get_k_vals(var_list)
+    var_list <- determine_independence(var_list)
+
+  }
+
+  return(var_list)
+}
+
+
+#' Find Neighbors
+#'
+#' This function finds all the neighbors for a particular node.
+#' A neighbor is defined as the set of parents, children, and spouses of the node.
+#'
+#' @param t the node of interest
+#' @param true_dag provides the adjacency matrix for the true DAG
+#' Note for the DAG adjacency matrix: A_{ij}=1 if i -> j
+
+
+find_neighbors <- function(t,true_dag){
+  # Parents of node t
+  parents <- which(true_dag[,t]==1)
+  # Children of node t
+  children <- which(true_dag[t,]==1)
+  # Spouses share the same children
+  spouses <- c()
+  for (c in children){ # loop through all of t's children
+    # Find all parents of current child
+    potential <- which(true_dag[,c]==1)
+    # Add all parents that are not t to spouses
+    spouses <- c(spouses,potential[potential!=t])
+  }
+  return(unique(c(parents,children,spouses)))
+}
+
+#### Get potential separating sets
+
+get_k_vals <- function(var_list){
+
+  if (var_list$l==0){
+    var_list$kvals <- NULL
+  } else if (length(var_list$adj)==1){
+    var_list$kvals <- matrix(var_list$adj,nrow = 1,ncol = 1)
+  } else {
+
+    # If there is more than one possible adjacent node
+    # build our various ksets for conditional independence
+    var_list$kvals <- combn(var_list$adj,var_list$l)
+
+  }
+
+
+  return(var_list)
+
+}
