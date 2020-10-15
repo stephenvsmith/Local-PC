1fd60907a9476632ffbbedfb2258d1820cf026f4
Stephen Smith <stephensmith13424@gmail.com>
2020-10-14 23:42:12 -0700

    Fixed a bug for the sample local pc by making sure the matrices are of the right data type

--- R/localpc.R
+++ R/localpc.R
@@ -29,11 +29,15 @@ localpc <- function(data=NULL,true_dag=NULL,target,G=NULL,lmax=3,tol=0.01,
 
 localpc_cpp <- function(data=NULL,true_dag=NULL,target,G=NULL,lmax=3,tol=0.01,
                         verbose = TRUE,verbose_small=TRUE){
+  node_names <- colnames(data)
   if (is.data.frame(data)){
-    node_names <- colnames(data)
     data <- as.matrix(data)
   }
   
-  return(pc_sample_cpp(true_dag,data,node_names,lmax,1-tol,verbose,verbose_small))
+  if (is.data.frame(true_dag)){
+    true_dag <- as.matrix(true_dag)
+  }
+  
+  return(pc_sample_cpp(true_dag,data,target,node_names,lmax,1-tol,verbose,verbose_small))
 
 }
